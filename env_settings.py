4269165a944330bd0b0cc170c288ed36157e27c5
David Brown <dbrown92700@gmail.com>
2024-02-27 17:44:02 -0500

    fixed env_settings.py for Windows

--- env_settings.py
+++ env_settings.py
@@ -3,19 +3,16 @@ import dotenv
 from cryptography.fernet import Fernet
 import os
 from getpass import getpass
+from pathlib import Path
 
 setting_key = b'dUxSOp1SDOKvfUt7oGNpQcMFgehFFJoVXWTHngyq1f8='
-basedir = os.path.abspath(__file__).removesuffix('/env_settings.py')
+basedir = Path(os.path.abspath(__file__)).parent
 configdir = basedir
 
 
 def get_setting(variable, prompt, secret):
     # Get environment variable or prompt user
-    dotenv.load_dotenv(f'{configdir}/.env')
-    # if os.path.exists(f'{configdir}/.env'):
-    #     dotenv.load_dotenv(f'{configdir}/.env')
-    # else:
-    #     open(f'{configdir}/.env', 'w').close()
+    dotenv.load_dotenv(configdir / '.env')
     try:
         response = os.environ[variable]
         if secret:
@@ -26,11 +23,11 @@ def get_setting(variable, prompt, secret):
     except KeyError:
         if secret:
             response = getpass(prompt)
-            dotenv.set_key(f'{configdir}/.env', variable,
+            dotenv.set_key(configdir / '.env', variable,
                            Fernet(setting_key).encrypt(bytes(response, 'ascii')).decode('ascii'))
         else:
             response = input(prompt)
-            dotenv.set_key(f'{configdir}/.env', variable, response)
+            dotenv.set_key(configdir / '.env', variable, response)
     return response
 
 
@@ -39,6 +36,6 @@ vmanage_user = get_setting('VMANAGE_USER', 'Input admin username: ', False)
 vmanage_password = get_setting('VMANAGE_PASSWORD', 'Input admin password: ', True)
 
 if input('Type "reset" to clear the settings, or anything else to proceed: ') == 'reset':
-    os.remove(f'{configdir}/.env')
+    os.remove(configdir / '.env')
     print('Settings cleared.  Restart script to proceed.')
     exit()
